e4b434f0c66012b0407cc74dcb8ccc237d9fc895
hakimomich <giroufle365@outlook.fr>
2021-06-08 14:40:25 +0100

    versionf

--- myjava.js
+++ myjava.js
@@ -2,16 +2,16 @@
 
 function produitChoisi()
 {
-    fetch("https://hakimomich.github.io/BoutalebHakim_5_04062021/")
+    fetch("https://hakimomich.github.io/BoutalebHakim_5_04062021/index.html")
     .then( res=>res.json())
     .then( data => 
     {
-
-            
-    }) 
-}
             const mycho = document.getElementsByClassName("bund__legend__button")
 
             mycho.addEventListener('click',function(){
 
                 document.location.href="https://hakimomich.github.io/BoutalebHakim_5_04062021/produit.html";
+            })  
+    }) 
+}
+function produitChoisi();
