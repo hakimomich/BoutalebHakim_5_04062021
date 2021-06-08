e9b33bfe63b1cab67cae5f63f120db8c9a475641
hakimomich <giroufle365@outlook.fr>
2021-06-08 14:41:49 +0100

    versionfg

--- myjava.js
+++ myjava.js
@@ -6,12 +6,13 @@ function produitChoisi()
     .then( res=>res.json())
     .then( data => 
     {
-            const mycho = document.getElementsByClassName("bund__legend__button")
+ 
+    }) 
+}
+const mycho = document.getElementsByClassName("bund__legend__button")
 
             mycho.addEventListener('click',function(){
 
                 document.location.href="https://hakimomich.github.io/BoutalebHakim_5_04062021/produit.html";
             })  
-    }) 
-}
 function produitChoisi();
