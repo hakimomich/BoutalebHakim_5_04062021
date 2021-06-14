5f509c7d7b9a636f776aecb782a86d13b045b2ec
hakimomich <giroufle365@outlook.fr>
2021-06-15 00:37:11 +0100

    versiondfg

--- myjava.js
+++ myjava.js
@@ -1,18 +1,17 @@
 
 
-function produitChoisi()
+function getcam()
 {
-    fetch("https://hakimomich.github.io/BoutalebHakim_5_04062021/index.html")
+    fetch("http://localhost:3000/api/cameras")
     .then( res=>res.json())
     .then( data => 
     {
- 
-    }) 
+        
+    }
 }
-const mycho = document.getElementsByClassName("bund__legend__button")
 
-            mycho.addEventListener('click',function(){
-
-                document.location.href="https://hakimomich.github.io/BoutalebHakim_5_04062021/produit.html";
-            })  
-function produitChoisi();
+let cams = document.getElementById('camera')
+        const myconst = document.getElementsByClassName("bund__legend__button");
+        myconst.AddEvenListener('click', function(){
+            location.href='https://hakimomich.github.io/BoutalebHakim_5_04062021/produit.html'
+        })
