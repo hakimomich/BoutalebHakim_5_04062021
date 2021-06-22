00f0383c6afddf283a1f00126e02b4a03891196e
hakimomich <giroufle365@outlook.fr>
2021-06-22 09:20:25 +0100

    versionff

--- front-end/myjava.js
+++ front-end/myjava.js
@@ -1,4 +1,11 @@
+let produit;
+let panier = [];
 
+let chainePanierStock = localStorage.getItem('panier');
+if(chainePanierStock != null)
+{
+    panier = JSON.parse(chainePanierStock);
+}
 
 function getcam()
 {
@@ -30,67 +37,35 @@ function getDetailCam() {
         let params = (new URL(document.location)).searchParams;
         let idde = params.get('id')
 
-    fetch("http://localhost:3000/api/cameras")
+    fetch("http://localhost:3000/api/cameras/"+ idde)
     .then( res=>res.json())
-    .then( cameras => 
+    .then( camera => 
     {
-        console.log(cameras);
-        for(camera of cameras)
+        console.log(camera);
+        produit = camera;
+        document.querySelector('.produit__img img').src=camera.imageUrl;
+        document.querySelector('.produit__name').innerHTML= `Nom : `+camera.name;
+        document.querySelector('.produit__texte').innerHTML=`Description : `+camera.description;
+        document.querySelector('.produit__prix').innerHTML=`Prix : `+camera.price;
+        for( option of camera.lenses)
         {
-            if(idde == camera._id){
-            document.getElementById('produit').innerHTML +=`
-            <div class="produit__img">
-                <img src="${camera.imageUrl}" alt="mon appareil" />
-            </div>
-            <div class="produit__text">
-            <div class="produit__name">Modèle : ${camera.name}</div>
-            <div class="produit__texte">Description : ${camera.description}</div>
-            <div class="produit__prix">Le prix est de : ${camera.price}</div> 
-            <div class="produit__choix">
-            <label>taille Objectif :</label>
-            <select name="produit__select" id="">
-            <option>optic 25</option>
-            <option>optic 35</option>
-            <option>optic 45</option>
-            </select>
-            </div>
-            <div class="buttontwo">
-                <a href="recap.html?id=${camera._id}" /><button>Ajouter au panier</button></a>
-            </div>
-            </div>
+            document.querySelector('.produit__select').innerHTML+= `
+            <option>${option}</option>
             `;
-        }}   
+        }
     })
 }
 function localstor() 
 {
-    let params = (new URL(document.location)).searchParams;
-    let iddde = params.get('id')
-
-    fetch("http://localhost:3000/api/cameras")
-    .then( res=>res.json())
-    .then( cameras => 
-    {
-        console.log(cameras);
-        for(camera of cameras)
-        { 
-            if(iddde == camera._id)
-        {
-            localStorage.setItem('nom', camera.name)
-            localStorage.setItem('prix', camera.price)
-            
-            document.getElementById('macommande').innerHTML +=`
-            <h4>Ma Commande :</h4>
-            <div class="element"> 
-            <div class="element__nom">Produit : ${localStorage.getItem('nom')}</div>
-            <span class="element__prix">Prix : ${localStorage.getItem('prix')}</span>
-            <i class="fas fa-trash-alt"></i>
-            </div>
-            <h4>Montant total :</h4>
-            `;
-        }
-       }
-    })
+    let commande = {
+        _id: produit._id,
+        name: produit.name,
+        price: produit.price,
+        choix: document.querySelector('.produit__select').value
+    };
+    panier.push(commande);
+    let chainePanier = JSON.stringify(panier);
+    localStorage.setItem('panier', chainePanier);
 }
 
 
