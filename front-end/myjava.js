d20b723d908e113a16a667c795a3a9f2cf4d7e82
hakimomich <giroufle365@outlook.fr>
2021-06-30 14:00:46 +0100

    versionf

--- front-end/myjava.js
+++ front-end/myjava.js
@@ -1,10 +1,11 @@
 let produit;
 let panier = [];
+let tabdonne = [];
 
 let chainePanierStock = localStorage.getItem('panier');
 if(chainePanierStock != null)
 {
-    panier = JSON.parse(chainePanierStock);
+    panier = JSON.parse(chainePanierStock);  
 }
 
 function getcam()
@@ -16,13 +17,14 @@ function getcam()
         console.log(cameras);
         for(camera of cameras)
         {
+            let pricu = (camera.price)/100;
             document.getElementById('secamera').innerHTML += `
             <div class="bund">
                 <div class="bund__imgSection">
                 <a href=""><img src="${camera.imageUrl}" /></a>
                 </div>
                 <div class="bund__legend">
-                    <p class="bund__legend__p">${camera.name}, le prix est de :${camera.price}</p>
+                    <p class="bund__legend__p">${camera.name}, le prix est de : ${pricu} &euro;</p>
                     <a href="produit.html?id=${camera._id}"><button id="buttonone" type="button" class="bund__legend__button">Personalisez</button></a>
                 </div>
             </div>
@@ -43,10 +45,13 @@ function getDetailCam() {
     {
         console.log(camera);
         produit = camera;
+
+        let pricer = (camera.price)/100;
+
         document.querySelector('.produit__img img').src=camera.imageUrl;
         document.querySelector('.produit__name').innerHTML= `Nom : `+camera.name;
         document.querySelector('.produit__texte').innerHTML=`Description : `+camera.description;
-        document.querySelector('.produit__prix').innerHTML=`Prix : `+camera.price;
+        document.querySelector('.produit__prix').innerHTML=`Prix : `+ pricer + ' ' + '&euro;';
         for( option of camera.lenses)
         {
             document.querySelector('.produit__select').innerHTML+= `
@@ -55,6 +60,7 @@ function getDetailCam() {
         }
     })
 }
+
 function localstor() 
 {
     let commande = {
@@ -64,8 +70,67 @@ function localstor()
         choix: document.querySelector('.produit__select').value
     };
     panier.push(commande);
+    
     let chainePanier = JSON.stringify(panier);
     localStorage.setItem('panier', chainePanier);
 }
 
+function affichePanier() 
+{  
 
+    let prod = document.getElementById('commandes')
+    for( let i=0;i<panier.length;i++)
+    {  
+        prod.innerHTML += `
+        <tr>
+            <td>${panier[i].name}</td>
+            <td>${panier[i].price/100}&euro;</td>
+            <td>
+                <button type="button" class="btn btn-primary rounded-pill" onclick="supprimePanier(${i})">
+                    <i class="fas fa-trash"></i>
+                </button>
+            </td>
+        </tr> 
+        `;   
+    /* "Produit : " + panier[i].name + " " + "Prix : " + panier[i].price;*/
+        
+    }    
+}
+
+function supprimePanier(i)
+{
+    panier.splice(i,1);
+    let pan = JSON.stringify(panier);
+    localStorage.setItem('panier', pan);
+    window.location.reload();
+}
+
+function recupdonnee() 
+{  
+    let donnee =
+    {
+         pren : document.querySelector('.prenom').value,
+         nom : document.querySelector('.nom').value,
+         address : document.querySelector('.adresse').value,
+         ville : document.querySelector('.ville').value,
+         mail :  document.querySelector('.mail').value
+    }  
+    
+    tabdonne.push(donnee);
+}
+
+function getRandomInt(max)
+{
+    return Math.floor(Math.random()*max);
+}
+
+function recapfinal()
+{
+    let recap = document.querySelector('.recap-final__numcom')
+    recap.innerHTML = `  
+    Nom : ${tabdonne.nom + '' + tabdonne.pren}        
+    <h5>Recapitulatif de vôtre commande : ${affichePanier()}</h5>
+    <h4>Numero de commande :</h4>${getRandomInt(max)}
+    <h4>Montant total de vôtre commande : </h4>${(produit.price)+=price[i]}
+    `;
+}
